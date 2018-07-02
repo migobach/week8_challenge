78dee02bd1450f9cd43d58a44ae058a1f008290a
Michael Bach <migobach@gmail.com>
2018-07-01 22:29:22 -0600

    add menu

--- index.rb
+++ index.rb
@@ -1,6 +1,6 @@
 require 'pry'
 require 'httparty'
-require 'json'
+require 'colorize'
 
 
 class BrainTeaser
@@ -8,13 +8,43 @@ class BrainTeaser
   base_uri 'http://json-server.devpointlabs.com/api/v1/users'
   
   def initialize(users, page)
-    @users = { query: { site: users, page: page } }
+    @users = { query: { site: users } }
+  end
+
+  def menu 
+    puts `clear`
+    puts "Make a selection:"
+    puts "1: List all users"
+    puts "2: List a single user"
+    puts "3: Add a user" 
+    puts "4: Edit a user" 
+    puts "5. Delete a user"
+
+    input = gets.strip.to_i 
+    selection 
+  end
+
+  def selection(input)
+    if ( input == 1 )
+      listUsers
+    elsif ( input == 2 )
+      singleUser
+    elsif ( input == 3 )
+      addUser
+    elsif ( input == 4 )
+      editUser
+    elsif ( input == 5 )
+      deleteUser
+    else 
+      puts "Invalid, try again"
+      menu
+    end
   end
   
-  def getUsers
-    list = self.class.get("/", @users) #.parsed_response - maybe need to add this and remove the JSON gem - do I need to use HTTParty.put("#{URL variable}")
-    puts list
-
+  def listUsers
+    puts "Here are all the users"
+    # list = self.class.get("/", @users)
+    # puts list
   #   list.each do 
   #     { |u| print list[u].values_at("first_name") }
   #   end
@@ -22,10 +52,23 @@ class BrainTeaser
   end
 
   def singleUser
-    puts "insert a user id"
-    id = gets.strip.to_i
-    single = self.class.get("/#{id}", @users)
-    puts single
+    puts "Insert a user id"
+    # id = gets.strip.to_i
+    # single = self.class.get("/#{id}", @users)
+    # puts single
+  end
+
+  def addUser
+    puts "Add a user"
+    # amke an axios post call to a single id
+  end
+
+  def editUser
+    puts "Edit user"
+  end 
+
+  def deleteUser
+    puts "Delete user"
   end
 end
 
@@ -33,5 +76,9 @@ end
 # puts stack_exchange.questions
 # puts stack_exchange.users 
 brain_teaser = BrainTeaser.new("DPLusers", 1)
-puts BrainTeaser brain_teaser.getUsers
-puts BrainTeaser brain_teaser.singleUser
+puts brain_teaser.menu
+
+
+
+#TODO
+#.parsed_response["columb header"] - maybe need to add this and remove the JSON gem - do I need to use HTTParty.put("#{URL variable}")
